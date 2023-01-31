79d0ce3dcfe2c6be99df2157384192ea8745466b
Raja Kumar <100175804+razzput007@users.noreply.github.com>
2023-01-31 09:42:30 +0530

    Update dom.js

--- dom.js
+++ dom.js
@@ -55,3 +55,53 @@ var h1=document.querySelector('header h1');
 console.log(newDiv);
 
 container.insertBefore(newDiv,h1);
+var form=document.getElementById("addForm");
+var itemList=document.getElementById("items");
+var filter = document.getElementById('filter');
+// add submit events
+form.addEventListener("submit",addItem);
+// add delete events
+itemList.addEventListener("click",removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
+function addItem(e){
+    e.preventDefault();
+
+    var newItem=document.getElementById("item").value;
+
+    var li=document.createElement("li");
+    li.className="list-group-item";
+    li.appendChild(document.createTextNode(newItem));
+
+    var deleteBtn=document.createElement('button')
+    deleteBtn.className="btn btn-danger btn-sm float-right delete";
+    deleteBtn.appendChild(document.createTextNode("X"));
+    li.appendChild(deleteBtn);
+    itemList.appendChild(li);
+}
+
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are You Sure?')){
+          var li = e.target.parentElement;
+          itemList.removeChild(li);
+        }
+      }
+}
+
+// Filter Items
+function filterItems(e){
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // Get lis
+    var items = itemList.getElementsByTagName('li');
+    // Convert to an array
+    Array.from(items).forEach(function(item){
+      var itemName = item.firstChild.textContent;
+      if(itemName.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
